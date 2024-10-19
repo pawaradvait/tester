09cb5be3ce10c1f81d69eb54619cf92e3f9a3b62
advait pawar <pawaradvait2003@gmail.com>
2024-10-19 19:12:25 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="birthday-card">
+        <div class="message-box">
+          <h1>Happy Birthday , Harsh</h1>
+          <p>
+            Wishing you a day filled with love, laughter, and happiness. May all
+            your dreams come true!
+          </p>
+          <p class="signature">With love, Advait</p>
+        </div>
+        <div class="balloons">
+          <div class="balloon red"></div>
+          <div class="balloon blue"></div>
+          <div class="balloon green"></div>
+        </div>
+      </div>
+    </>
   );
 }
 
